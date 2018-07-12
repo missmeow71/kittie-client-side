3e156ffbe2b314444566c61b1d49ea178a8b94b1
missmeow71 <sensuousdesigner@gmail.com>
2018-07-12 15:58:04 -0600

    added css styling

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -7,8 +7,8 @@ class Splash extends Component {
             <div>
                 <section className="intro">
                     <h1 className="subtitle">Show Me Your Kitties!</h1>
-                    <p className="description">Welcome to Show Me Your Kitties! The app where you can view and add images and comments of your fabulous felines! 
-                       You can even upvote your favorites! 
+                    <p className="description">Welcome to Show Me Your Kitties! The app where you can view, add images and comments of fabulous felines! 
+                       You can even upvote your favorites! Also, Please consider donating to our local feline rescue! 
                     </p>
                     <Link className="goto" to="/CatCard"> 
                     < button className="enter">MEOW!</button>
